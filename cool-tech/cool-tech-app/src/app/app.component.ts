5687ab798e57dad18648c07471b7f216d47d691d
Varbanov <stavar01@protiviti.com>
2022-03-09 22:09:42 +0200

    router + pages components created

--- cool-tech/cool-tech-app/src/app/app.component.ts
+++ cool-tech/cool-tech-app/src/app/app.component.ts
@@ -8,3 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cool-tech-app';
 }
+
--- cool-tech/cool-tech-app/src/app/app.module.ts
+++ cool-tech/cool-tech-app/src/app/app.module.ts
@@ -4,26 +4,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
-import { MatMenuModule } from '@angular/material/menu'
-;
-import { MatCardModule } from '@angular/material/card'
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+  
 import { DevicesComponent } from './components/devices/devices.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { RequestComponent } from './request/request.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+
+const routes: Routes = [
+  { path: 'browse-tech', component: DevicesComponent },
+  { path: 'our-services',component: ServicesComponent }
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DevicesComponent
+    DevicesComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    RequestComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatMenuModule,
     MatCardModule,
-
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
