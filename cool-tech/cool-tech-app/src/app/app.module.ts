984ff2594613660ddb964b498d7d39aa5da098c4
Varbanov <stavar01@protiviti.com>
2022-03-26 21:35:33 +0200

    cleanup

--- cool-tech/cool-tech-app/src/app/app.module.ts
+++ cool-tech/cool-tech-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './services/authconfig.interceptor.js';
+
 
 
 import { AppComponent } from './app.component';
