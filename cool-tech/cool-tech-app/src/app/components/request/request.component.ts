ddb8e2776c8f736e5e74981c8b387f97cd340465
Varbanov <stavar01@protiviti.com>
2022-03-26 21:14:15 +0200

    request form

--- cool-tech/cool-tech-app/src/app/components/request/request.component.ts
+++ cool-tech/cool-tech-app/src/app/components/request/request.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
   styleUrls: ['./request.component.css']
 })
-export class RequestComponent implements OnInit {
+export class RequestComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('requestForm') requestForm!: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+
+  }
+
+  onSubmit() {
+    alert(JSON.stringify(this.requestForm.value));
   }
 
 }
